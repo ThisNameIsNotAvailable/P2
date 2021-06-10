743b590109fcb82e7d7d71dbabe472f8ea07ed1c
ThisNameIsNotAvailable <aliaxeyka@gmail.com>
2021-06-10 11:52:50 +0200

    added destructors

--- zestaw7/main.cpp
+++ zestaw7/main.cpp
@@ -6,12 +6,19 @@ using namespace std;
 class Shape {
 protected:
     virtual void draw() = 0;
+public:
+    virtual ~Shape() {
+        cout << "Shape deleted" << endl;
+    }
 };
 
 class Circle: public Shape {
 public:
     virtual void draw() {
         cout << "It is a circle" << endl;
+    }
+    ~Circle() {
+        cout << "Circle deleted" << endl;
     }
 };
 
@@ -20,6 +27,10 @@ public:
     virtual void draw() {
         cout << "It is a square" << endl;
     }
+    
+    ~Square() {
+        cout << "Square deleted" << endl;
+    }
 };
 
 class Triangle: public Shape {
@@ -27,13 +38,21 @@ public:
     virtual void draw() {
         cout << "It is a triangle" << endl;
     }
+    
+    ~Triangle() {
+        cout << "Triangle deleted" << endl;
+    }
 };
 
 class Rodent {
 public:
-    virtual void whoAmI() // = 0; //zeby klasa byla czysto abstrakcyjna
-    {
-        cout << "Unknown type of rodent" << endl;
+    virtual void whoAmI() = 0; //zeby klasa byla czysto abstrakcyjna
+//    {
+//        cout << "Unknown type of rodent" << endl;
+//    }
+    
+    virtual ~Rodent() {
+        cout << "Rodent deleted" << endl;
     }
 };
 
@@ -42,6 +61,9 @@ public:
     virtual void whoAmI() {
         cout << "I am a mouse" << endl;
     }
+    ~Mouse() {
+        cout << "Mouse deleted" << endl;
+    }
 };
 
 class Gerbil: public Rodent {
@@ -49,19 +71,31 @@ public:
     virtual void whoAmI() {
         cout << "I am a gerbil" << endl;
     }
+    
+    ~Gerbil() {
+        cout << "Gerbil deleted" << endl;
+    }
 };
 
 class Hamster: public Rodent {
 public:
-    virtual void whoAmI() {
+    void whoAmI() {
         cout << "I am a hamster" << endl;
+    }
+    
+    ~Hamster() {
+        cout << "Hamster deleted" << endl;
     }
 };
 
 class BlueHamster: public Hamster {
 public:
-    virtual void whoAmI() {
+    void whoAmI() {
         cout << "I am a blue hamster" << endl;
+    }
+    
+    ~BlueHamster() {
+        cout << "BlueHamster deleted" << endl;
     }
 };
 
@@ -127,14 +161,46 @@ public:
     }
 };
 
+class A {
+public:
+    virtual void foo() {
+        cout << "foo(void) in A" << endl;
+    }
+    virtual void foo(int a) {
+        cout << "foo(int) in A" << endl;
+    }
+    virtual void foo(float a) {
+        cout << "foo(float) in A" << endl;
+    }
+};
+
+class B: public A {
+public:
+    void foo() {
+        cout << "foo(void) in B" << endl;
+    }
+//    void foo(int a) {
+//        cout << "foo(int) in B" << endl;
+//    }
+//    void foo(float a) {
+//        cout << "foo(float) in B" << endl;
+//    }
+};
 
 int main(int argc, const char * argv[]) {
+//    A* b = new B;
+//    float a = 2.5;
+//    b->foo();
+//    b->foo(2);
+//    b->foo(a);
+//    delete b;
+    
 //==================ZADANIE 1=================================
     
     Circle circle;
     Square square;
     Triangle triangle;
-    
+
     circle.draw();
     square.draw();
     triangle.draw();
@@ -150,21 +216,24 @@ int main(int argc, const char * argv[]) {
     rodents[1] = &g;
     Hamster h;
     rodents[2] = &h;
-    
+
     for(int i = 0; i < 3; i++)
         rodents[i]->whoAmI();
     
     cout << "===================================================" << endl;
-    
+
     vector<Rodent*> rodentsVector;
     Mouse m2;
     Gerbil g2;
     Hamster h2;
-    
+    BlueHamster bh2;
+
     rodentsVector.push_back(&m2);
     rodentsVector.push_back(&g2);
     rodentsVector.push_back(&h2);
-    
+    rodentsVector.push_back(&bh2);
+
+
     for(int i = 0; i < rodentsVector.size(); i++)
         rodentsVector[i]->whoAmI();
     
@@ -173,16 +242,16 @@ int main(int argc, const char * argv[]) {
 //==================ZADANIE 3=================================
     
     Tower tower("tower1");
-    
+
     Helicopter h1("h1");
     Plane p1("p1");
     Drone d1("d1");
-    
+
     tower.addAircraft(h1);
     tower.addAircraft(p1);
     tower.addAircraft(d1);
-    
+
     tower.sendMessage("The weather is so nice today\n");
-    
+
     return 0;
 }
